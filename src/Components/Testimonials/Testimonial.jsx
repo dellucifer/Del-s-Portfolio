b629753e62d1c0dde36169006c6b508c0a9cc97e
Dellucifer <pg99285@gmail.com>
2023-11-22 01:43:32 +0530

    [UPDATE] Cards in Testimonial

--- src/Components/Testimonials/Testimonial.jsx
+++ src/Components/Testimonials/Testimonial.jsx
@@ -4,16 +4,19 @@ import data from "./testimonialData";
 
 const Testimonial = () => (
     <div className="container">
+      <h1 style={{fontSize: "60px"}}>Testimonials</h1>
+      <div className="cards">
       {data.map((data, i) => (
         <Card
         name={data.author}
         title={data.title}
         image={data.img}
-        text={data.testimonial}
+        text={data.testimonial.length > 280 ? data.testimonial.slice(0, 280) + '...' : data.testimonial}
         linkedin={data.linkedin}
         key={i}
-      />
-      ))}
+        />
+        ))}
+        </div>
     </div>
 );
 
--- src/Components/Testimonials/testimonialData.js
+++ src/Components/Testimonials/testimonialData.js
@@ -1,7 +1,7 @@
 const data = [
   {
     author: "Sandeep Kumar",
-    title: "Co-Founder HackersLounge, YouTuber 50K+",
+    title: "Co-Founder HackersLounge, YouTuber 50K+ Fam",
     img: "https://media.licdn.com/dms/image/D4D03AQFVUES-Xq2-wA/profile-displayphoto-shrink_800_800/0/1697779194688?e=1705536000&v=beta&t=kRoWlkw19eZ8bly9p41VhmCkdQ6oXUks2AM3c2Waa18",
     testimonial:
       "I had the privilege of mentoring Priyanshu, and I'm impressed by their expertise in cybersecurity. They have a keen analytical mind and a strong commitment to excellence. Priyanshu is a valuable asset for any team or organization seeking top-tier cybersecurity talent. All the best",
@@ -9,7 +9,7 @@ const data = [
   },
   {
     author: "Ankit Kumar",
-    title: "5 star Upwork, Winner SIH@20",
+    title: "5 star Freelancer at Upwork, Winner SIH@20",
     img: "https://media.licdn.com/dms/image/D4E03AQF1fpe66UarWw/profile-displayphoto-shrink_400_400/0/1682540806333?e=1705536000&v=beta&t=OeRQ4xphvtLf0e7NY0HPgMrQaHSAxrDoCKkM2w8PLls",
     testimonial:
       "I had the privilege of working with Priyanshu when I served as the Student Co-ordinator for our college's software development club. Even then, his passion for technology, especially in the realm of cybersecurity, was evident and commendable. His depth of knowledge in this domain always stood out, making him a valuable asset to our team. Now, as the Student Co-ordinator, I've observed Priyanshu take on a pivotal role with zeal and dedication. Under his leadership, the club has not only thrived but has also seen a surge in activities and events, ensuring that the members are consistently exposed to the latest in the tech world. His knack for organization and his unwavering commitment to promoting technological literacy among his peers is truly commendable. I firmly believe that Priyanshu's unique combination of leadership skills and technical expertise makes him an asset to any team or project he undertakes. He is undoubtedly one of the brightest and most dedicated individuals I've had the pleasure of working with, and I wholeheartedly recommend him for any future endeavors.",
@@ -17,7 +17,7 @@ const data = [
   },
   {
     author: "Sahil Sharma",
-    title: "SDE-1 at Toast, Candidate Master at Codeforces",
+    title: "SDE at Toast, Candidate Master at Codeforces",
     img: "https://media.licdn.com/dms/image/C4D03AQEWsvq0xwB6QQ/profile-displayphoto-shrink_400_400/0/1663788378882?e=1705536000&v=beta&t=GHpLR9xC2rWXHUpl6hNnm3d_bXvRsrpJ1jyqUk106Mw",
     testimonial:
       "Priyanshu has a zeal for cyber-security and is also a quick learner, so he can quickly adapt to any tech stack that is thrown at him. So he will be suited for mostly all the developer roles, but I think he would shine most in the cyber security space. Along with this, he has great leadership skills that he showed while managing the computer society of around 100 students.",
